22b0bcf96f1962dacac20f2d904d8756b9e4e86e
itThreeDay <2939496363@qq.com>
2020-06-01 10:45:59 +0800

    根组件app.js与入口文件index.js各司其职

---
+++ myapp/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class app extends React.Component{
+    render(){
+        return (
+            <div
+            className="c1"
+            >
+                this is my frist react Component
+            </div>
+        )
+    }
+}
+
+export default app
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./app";
 import "./index.css"
 
-class FristCom extends React.Component{
-    render(){
-        return (
-            <div
-            className="c1"
-            >
-                this is my frist react Component
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<FristCom/>,document.getElementById("root"));
+ReactDOM.render(<App/>,document.getElementById("root"));
 
